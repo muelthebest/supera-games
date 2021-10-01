2cdaa52fec80175e40f50939c6201fc054b66079
Samuel Elias <samuel.elias.dev@gmail.com>
2021-10-01 15:12:56 -0300

    fix(src): aumenta o escopo de QueryClientProvider

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
 
 ReactDOM.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
             <CssBaseline />
             <App />
-        </QueryClientProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
