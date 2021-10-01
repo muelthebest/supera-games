4da8878926853e062b63299a0c13973c27bef1e7
Samuel Elias <samuel.elias.dev@gmail.com>
2021-10-01 15:13:50 -0300

    feat(context): formata a moeda para real

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -5,23 +5,38 @@ import { useQuery } from "react-query";
 export const ProductsContext = createContext({});
 
 export const ProductsProvider = (props) => {
+    const [products, setProducts] = useState([]);
 
-    const { data: products , error, isLoading} = useQuery("products", () => {
-        return axios("http://localhost:3001/products")
-    },
-    {
-        retry: false,
-        refetchOnWindowFocus: false,
-    }
-    )
+    const { data, error, isLoading } = useQuery(
+        "products",
+        () => {
+            return axios("http://localhost:3001/products").then((response) => {
+                const productFormatted = response.data.map((product) => {
+                    return {
+                        ...product,
+                        price: product.price.toLocaleString("pt-br", {
+                            style: "currency",
+                            currency: "BRL",
+                        }),
+                        nameFormatted: `${product.name.substring(0, 20)}... `
+                    };
+                });
+                setProducts(productFormatted);
+            });
+        },
+        {
+            retry: false,
+            refetchOnWindowFocus: false,
+        }
+    );
 
-    if(error) return <h1> Error: {error}, try again!</h1>
-    
-    if(isLoading) return <h1> Loading ...</h1>
-    
+    if (error) return <h1> Error: {error}, try again!</h1>;
+
+    if (isLoading) return <h1> Loading ...</h1>;
+
     return (
         <ProductsContext.Provider value={{ products }}>
             {props.children}
         </ProductsContext.Provider>
-    )
-}
+    );
+};
